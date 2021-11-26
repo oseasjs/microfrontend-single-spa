53504864dcf5ced0b52572679683ff49f57b502c
Oséas Santana <oseasjs@gmail.com>
2021-11-26 17:30:40 -0300

    Adapting to run on github pages with github actions

--- root-config/src/poc-root-config.js
+++ root-config/src/poc-root-config.js
@@ -1,10 +1,30 @@
 import { registerApplication, start } from "single-spa";
+import axios from 'axios'
 
-// NOTE: Json used as response payload on mocky.io is avaliable in: ./mocky-io-sample/applications.json
-fetch('https://run.mocky.io/v3/7f53a28a-3271-434f-bba6-0545d0b5eb7b')
-  .then(resp => resp.json())
-  .then(data => {
-    data.applications.forEach(app => {
+
+// NOTE: Json used as response payload on mocky.io is avaliable in: ./public/applications.json
+// fetch('https://run.mocky.io/v3/7f53a28a-3271-434f-bba6-0545d0b5eb7b')
+//   .then(resp => resp.json())
+//   .then(data => {
+//     data.applications.forEach(app => {
+//       registerApplication({
+//         name: app.name,
+//         app: () => System.import(app.package),
+//         activeWhen: app.exact 
+//           ? (location) => location.pathname === app.activeWhen 
+//           : app.activeWhen
+//       });
+//     })
+//   })
+//   .finally(() => {
+//     start({
+//       urlRerouteOnly: true,
+//     });
+//   })
+
+  axios('https://oseasjs.github.io/microfrontend-single-spa/root-config/applications.json')
+  .then(resp => {
+    resp.data.applications.forEach(app => {
       registerApplication({
         name: app.name,
         app: () => System.import(app.package),
