68ad8426a166186e68caede545dcbc0fc4803456
Oséas Santana <oseasjs@gmail.com>
2021-11-26 14:08:12 -0300

    Adding gh-pages

--- root-config/src/poc-root-config.js
+++ root-config/src/poc-root-config.js
@@ -4,13 +4,13 @@ import { registerApplication, start } from "single-spa";
 fetch('https://run.mocky.io/v3/7f53a28a-3271-434f-bba6-0545d0b5eb7b')
   .then(resp => resp.json())
   .then(data => {
-    data.applications.forEach(d => {
+    data.applications.forEach(app => {
       registerApplication({
-        name: d.name,
-        app: () => System.import(d.package),
-        activeWhen: d.exact 
-          ? (location) => location.pathname === d.activeWhen 
-          : d.activeWhen
+        name: app.name,
+        app: () => System.import(app.package),
+        activeWhen: app.exact 
+          ? (location) => location.pathname === app.activeWhen 
+          : app.activeWhen
       });
     })
   })
